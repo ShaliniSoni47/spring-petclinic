11e285ee0002f96236b1fa2fe2337192be2603ee
ShaliniSoni47 <51402891+ShaliniSoni47@users.noreply.github.com>
2019-06-11 16:16:23 +0530

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -18,7 +18,7 @@ package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-some text
+
 /**
  * PetClinic Spring Boot Application.
  *
